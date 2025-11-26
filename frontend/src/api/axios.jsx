0b886e56c68989bba0e21bb00171b39ba7ba8ef6
faizan650 <150579105+faizan650@users.noreply.github.com>
2025-11-26 17:12:43 +0530

    Update axios.jsx

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL,   // Render backend URL
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
